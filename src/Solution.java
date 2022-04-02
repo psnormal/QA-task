2c0fae1da36c7d3a0587cf06ef1d79b03227ae60
Anna Torgunakova <annatorgun@mail.ru>
2022-04-02 17:03:38 +0700

    add solution of task

---
+++ src/Solution.java
@@ -0,0 +1,69 @@
+import java.util.function.Function;
+
+public class Solution {
+    public static String diag1Sym(String strng) {
+        char[][] let = returnArray(strng);
+        int n = let.length;
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result += let[j][i];
+            }
+            if (i < n - 1)
+            {
+                result += "\n";
+            }
+        }
+        return result;
+    }
+
+    public static String rot90Clock(String strng) {
+        char[][] let = returnArray(strng);
+        int n = let.length;
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                result += let[j][i];
+            }
+            if (i < n - 1)
+            {
+                result += "\n";
+            }
+        }
+        return result;
+    }
+
+    public static String selfieAndDiag1(String strng) {
+        String[] strs = strng.split("\n");
+        char[][] let = returnArray(strng);
+        int n = let.length;
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            result += strs[i] + "|";
+            for (int j = 0; j < n; j++) {
+                result += let[j][i];
+            }
+            if (i < n - 1)
+            {
+                result += "\n";
+            }
+        }
+        return result;
+    }
+
+    public static String oper(Function<String, String> operator, String s) {
+        return operator.apply(s);
+    }
+
+    public static char[][] returnArray(String s) {
+        String[] strs = s.split("\n");
+        int n = strs.length;
+        char[][] letters = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                letters[i][j] = strs[i].charAt(j);
+            }
+        }
+        return letters;
+    }
+}
